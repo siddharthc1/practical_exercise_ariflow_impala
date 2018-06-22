4660aef7eb5d3b24f6ee1f3d37adfaf8cb12fb1b
siddharthc1 <39838707+siddharthc1@users.noreply.github.com>
2018-06-22 12:11:58 -0700

    Update airflow_task_script.py

--- airflow_task_script.py
+++ airflow_task_script.py
@@ -12,7 +12,7 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-dag = DAG('airflow_task_script', default_args=default_args, schedule_interval="*/3 * * * *", start_date=datetime.now() - timedelta(minutes=1))
+dag = DAG('airflow_task_script', default_args=default_args, schedule_interval=None, start_date=datetime.now() - timedelta(minutes=1))
 
 
 generating_the_data= BashOperator(
@@ -52,31 +52,31 @@ Drop_user_report_table= BashOperator(
 
 Create_user_report= BashOperator(
     task_id='Create_user_report',
-    bash_command=""" impala-shell -q "create table practical_exercise_1.user_report(user_id bigint, total_updates bigint, total_inserts bigint, total_deletes bigint, last_activity_type string, is_active boolean, upload_count bigint);"""",
+    bash_command=""" impala-shell -q "create table practical_exercise_1.user_report(user_id bigint, total_updates bigint, total_inserts bigint, total_deletes bigint, last_activity_type string, is_active boolean, upload_count bigint);" """,
     dag=dag)
 
 Insert_user_report= BashOperator(
     task_id='Insert_user_report',
     bash_command=""" impala-shell -q "insert into practical_exercise_1.user_report 
-select a.user_id, 
-COALESCE(b.co,0) as U, 
-COALESCE(c.co,0) as I, 
-COALESCE(d.co,0) as D, 
-COALESCE(e.co,NULL) as type, 
-COALESCE(f.co,FALSE) as bool, 
-COALESCE(g.co,0) as upload 
-from (select id as user_id from practical_exercise_1.user group by id) as a 
-left join (select user_id, count(user_id) as co from practical_exercise_1.activitylog where type='UPDATE' group by user_id) as b on a.user_id=b.user_id  
-left join (select user_id, count(user_id) as co from practical_exercise_1.activitylog where type='INSERT' group by user_id) as c on a.user_id=c.user_id 
-left join(select user_id, count(user_id) as co from practical_exercise_1.activitylog where type='DELETE' group by user_id) as d on a.user_id=d.user_id 
-left join (SELECT a.user_id, a.type as co FROM practical_exercise_1.activitylog a INNER JOIN (SELECT user_id, MAX(ti) as ti FROM practical_exercise_1.activitylog GROUP BY user_id ) AS b ON a.user_id = b.user_id AND a.ti = b.ti) as e on a.user_id=e.user_id 
-left join (select user_id, if(count(*) = 0, FALSE, TRUE) as co from practical_exercise_1.activitylog where ti between 1529351994 AND 1529351994-172800 group by user_id) as f on a.user_id=f.user_id 
-left join (select user_id, count(user_id) as co from practical_exercise_1.user_upload_dump group by user_id) as g on a.user_id=g.user_id;"""",
+﻿select a.user_id,
+COALESCE(b.co,0) as U,
+COALESCE(c.co,0) as I,
+COALESCE(d.co,0) as D,
+COALESCE(e.co,NULL) as type,
+COALESCE(f.co,FALSE) as bool,
+COALESCE(g.co,0) as upload
+from (select id as user_id from practical_exercise_1.user group by id) as a
+left join (select user_id, count(user_id) as co from practical_exercise_1.activitylog where type='UPDATE' group by user_id) as b on a.user_id=b.user_id
+left join (select user_id, count(user_id) as co from practical_exercise_1.activitylog where type='INSERT' group by user_id) as c on a.user_id=c.user_id
+left join(select user_id, count(user_id) as co from practical_exercise_1.activitylog where type='DELETE' group by user_id) as d on a.user_id=d.user_id
+left join (SELECT a.user_id, a.type as co FROM practical_exercise_1.activitylog a INNER JOIN (SELECT user_id, MAX(ti) as ti FROM practical_exercise_1.activitylog GROUP BY user_id ) AS b ON a.user_id = b.user_id AND a.ti = b.ti) as e on a.user_id=e.user_id
+left join (select user_id, if(count(*) = 0, FALSE, TRUE) as co from practical_exercise_1.activitylog where ti > $NOW-172800 group by user_id) as f on a.user_id=f.user_id
+left join (select user_id, count(user_id) as co from practical_exercise_1.user_upload_dump group by user_id) as g on a.user_id=g.user_id;" """,
     dag=dag)
 
 Insert_user_total= BashOperator(
     task_id='Insert_user_total',
-    bash_command=""" insert into practical_exercise_1.user_total select $NOW, sub1.t , case when sub2.t1 is NULL then sub1.t when sub2.t1 is not NULL then sub1.t-sub2.t1 end from (select count(distinct id) as t from practical_exercise_1.user)sub1, (select max(total_users) t1 from practical_exercise_1.user_total) sub2;"""",
+    bash_command=""" insert into practical_exercise_1.user_total select $NOW, sub1.t , case when sub2.t1 is NULL then sub1.t when sub2.t1 is not NULL then sub1.t-sub2.t1 end from (select count(distinct id) as t from practical_exercise_1.user)sub1, (select max(total_users) t1 from practical_exercise_1.user_total) sub2;" """,
     dag=dag)
 
 
